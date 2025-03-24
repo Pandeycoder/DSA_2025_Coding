7aad6382937e6f81bde055a2ad1939f7defb84c0
Rahul <rahul123@gmail.com>
2025-03-24 17:51:58 +0530

    update the code

--- DSA_Interview_2025/src/pattern/Solid_Rectangle.java
+++ DSA_Interview_2025/src/pattern/Solid_Rectangle.java
@@ -4,9 +4,10 @@ public class Solid_Rectangle {
 	
 	public static void main(String[] args) {
 		
-		
+		//outer loop - row observe
 		for(int i=0;i<3;i++)
 		{
+			// inner loop - col observe
 			for(int j=0;j<5;j++)
 			{
 			  System.out.print(" *");
---
+++ DSA_Interview_2025/src/pattern/Square.java
@@ -0,0 +1,24 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class Square {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter a number : -");
+		int n=sc.nextInt();
+		
+		for (int row = 0; row < n; row++) {
+			for (int col = 0; col < n; col++) {
+
+				System.out.print(" *");
+
+			}
+
+			System.out.println();
+		}
+
+	}
+}
